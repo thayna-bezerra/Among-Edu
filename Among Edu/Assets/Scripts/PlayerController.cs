c54abf81c63060d86301e1312e9f01b18d4685ce
thayna-bezerra <arraisthayna2106@gmail.com>
2022-08-17 11:21:57 -0300

    Movimentação Player

---
+++ Among Edu/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator Animation;
+
+    public float speed;
+    Rigidbody2D rgbd;
+
+    public bool isActive;
+
+    private void Start()
+    {
+        Animation = GetComponent<Animator>();
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        AnimStatus();
+    }
+
+    void MovePlayer()
+    {
+        Vector2 pos = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rgbd.velocity = pos * speed;
+
+        float a = Input.GetAxis("Horizontal");
+        float b = Input.GetAxis("Vertical");
+
+        if (a > 0 || b > 0)
+        {
+            isActive = true; //está ativo andando
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+
+        else if (a < 0 || b < 0)
+        {
+            isActive = true; //está ativo andando
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        else
+        {
+            isActive = false; //está parado
+        }
+    }
+
+    void AnimStatus()
+    {
+        if (isActive)
+        {
+            Animation.Play("PlayerCaminhando");
+
+        }
+
+        else
+        {
+            Animation.Play("PlayerParado");
+        }
+    }
+}
