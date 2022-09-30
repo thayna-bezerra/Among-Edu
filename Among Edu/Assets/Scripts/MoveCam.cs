c98fb1b396d81c199d778607c3b25da29d9a658c
thayna-bezerra <arraisthayna2106@gmail.com>
2022-09-30 16:33:15 -0300

    Background Inicial Parallax

---
+++ Among Edu/Assets/Scripts/MoveCam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    public float speed;
+
+    public GameObject point1, point2;
+    private Vector2 nextPos;
+
+    void Update()
+    {
+        if (transform.position == point1.transform.position)
+        {
+            nextPos = point2.transform.position;
+        }
+
+        if (transform.position == point2.transform.position)
+        {
+            nextPos = point1.transform.position;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, nextPos, speed);
+
+    }
+}
---
+++ Among Edu/Assets/Scripts/ParallaxForLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxForLevel : MonoBehaviour
+{
+    private float length;
+    private float StartPos;
+
+    public float ParallaxEffect;
+
+    void Start()
+    {
+        StartPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float RePos = transform.position.x * (1 - ParallaxEffect);
+        float Distance = transform.position.x * ParallaxEffect;
+
+        transform.position = new Vector3(StartPos + Distance, transform.position.y, transform.position.z);
+
+        if (RePos > StartPos + length)
+        {
+            StartPos += length;
+        }
+
+        else if (RePos < StartPos - length)
+        {
+            StartPos -= length;
+        }
+    }
+}
