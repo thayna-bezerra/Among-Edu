8e981dde87be338b6f19a3fa384a2679834c84e2
thayna-bezerra <arraisthayna2106@gmail.com>
2022-10-03 09:57:50 -0300

    Inicialização e criação da cutscene antes de entrar qualquer operação

--- Among Edu/Assets/Scripts/SystemControl/InicioControle.cs
+++ Among Edu/Assets/Scripts/SystemControl/InicioControle.cs
@@ -8,42 +8,94 @@ public class InicioControle : MonoBehaviour
     [Header("Panels")]
     public GameObject panelPrimeiraTela;
     public GameObject panelOpcoes;
+    public GameObject panelCutscene;
+
+    [Header("Astronautas")]
+    public GameObject a1;
+    public GameObject a2;
+    public GameObject a3;
+
+    public bool Adicao = false;
+    public bool Subtracao = false;
+    public bool Multiplicacao = false;
+    public bool Divisao = false;
 
     private void Start()
     {
        panelPrimeiraTela.SetActive(true);
        panelOpcoes.SetActive(false);
+       panelCutscene.SetActive(false);
+
+        a1.SetActive(true);
+        a2.SetActive(true);
+        a3.SetActive(true);
+
+        Adicao = false;
+        Subtracao = false;
+        Subtracao = false;
+        Divisao = false;
     }
 
     public void irParaPrimeiraTela()
     {
         panelPrimeiraTela.SetActive(true);
         panelOpcoes.SetActive(false);
+        panelCutscene.SetActive(false);
     }
 
     public void irParaOpcoes()
     {
         panelPrimeiraTela.SetActive(false);
         panelOpcoes.SetActive(true);
+        panelCutscene.SetActive(false);
     }
 
     public void sceneAdicao()
     {
-        SceneManager.LoadScene("Adicao");
+        StartCoroutine(chamaCutscene());
+        Adicao = true;
     }
 
     public void sceneSubtracao()
     {
-        SceneManager.LoadScene("Subtracao");
+        StartCoroutine(chamaCutscene());
+        Subtracao = true;
     }
 
     public void sceneMultiplicacao()
     {
-        SceneManager.LoadScene("Multiplicacao");
+        StartCoroutine(chamaCutscene());
+        Multiplicacao = true;
     }
 
     public void sceneDivisao()
     {
-        SceneManager.LoadScene("Divisao");
+        StartCoroutine(chamaCutscene());
+        Divisao = true;
+    }
+
+    IEnumerator chamaCutscene()
+    {
+        playCutscene();
+
+        yield return new WaitForSeconds(4f); 
+
+        if(Adicao == true)
+            SceneManager.LoadScene("Adicao");
+        if(Subtracao == true)
+            SceneManager.LoadScene("Subtracao");
+        if(Multiplicacao == true)
+            SceneManager.LoadScene("Multiplicacao");
+        if(Divisao == true)
+            SceneManager.LoadScene("Divisao");
+    }
+
+    public void playCutscene()
+    {
+        panelCutscene.SetActive(true);
+        panelOpcoes.SetActive(false);
+        a1.SetActive(false);
+        a2.SetActive(false);
+        a3.SetActive(false);
     }
 }
